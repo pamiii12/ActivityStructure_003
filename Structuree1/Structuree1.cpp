55bfa30bec7c466c09c1b4cda597cb817302ce57
Fahmi <114166295+pamiii12@users.noreply.github.com>
2023-04-05 15:51:59 +0700

    Add files via upload
    comit manual

---
+++ Structuree1/Structuree1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+struct Mahasiswa
+{
+	string NIM;
+	string Nama;
+	string Alamat;
+	int Umur;
+};
+
+int main()
+{
+	Mahasiswa mhs;
+
+	cout << "Masukkan NIM  = ";
+	cin >> mhs.NIM;
+	cout << "masukkan Nama = ";
+	cin >> mhs.Nama;
+	cout << "Masukkan Alamat = ";
+	cin >> mhs.Alamat;
+	cout << "Masukkan Umur = ";
+	cin >> mhs.Umur;
+
+	cout << "\nNIM = " << mhs.NIM;
+	cout << "\nNama= " << mhs.Nama;
+	cout << "\nAlamat = " << mhs.Alamat;
+	cout << "\nUmur = " << mhs.Umur;
+}
